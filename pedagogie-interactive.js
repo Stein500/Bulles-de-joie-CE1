e8b69e2ac6c4325d4476352771617bbfa58d6203
C. J. S. Stein <codjosamuelstein@gmail.com>
2025-11-15 16:11:59 +0100

    Delete pedagogie-interactive.js

--- pedagogie-interactive.js
+++
@@ -1,8 +0,0 @@
-// JS interactif pour pédagogie - Expand list items
-const listItems = document.querySelectorAll('.cycles-list li');
-listItems.forEach(li => {
-  li.addEventListener('click', () => {
-    li.classList.toggle('expanded');
-    // Ajoute description si besoin
-  });
-});
